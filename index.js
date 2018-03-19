2343c562ea40af9d3b25dc7abb792ed7e33bfba9
Mihaela <mihaelaagrigore@gmail.com>
2018-03-19 19:42:04 +0200

    pin 4

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var path = require('path');
 var gpio = require('rpi-gpio');
 
 //gpio.setup(7, gpio.DIR_OUT);
-gpio.setup(7, gpio.DIR_IN);
+gpio.setup(4, gpio.DIR_IN, readInput);
  
 function readInput() {
-    gpio.read(7, function(err, value) {
+    gpio.read(4, function(err, value) {
         console.log('The value is ' + value);
     });
 }
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 console.log(path.join(__dirname, 'public'));
 
 app.get('/', function(req, res){ 
-    gpio.read(7, function(err, value) {
+    gpio.read(4, function(err, value) {
         console.log('The value is ' + value);
         return res.render('index', {status: value});
     });
