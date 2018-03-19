848b8471d1d9a064cd4d707f50669713d179e3e4
Mihaela <mihaelaagrigore@gmail.com>
2018-03-19 20:00:09 +0200

    pin 7 = GPIO4

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ var path = require('path');
 var gpio = require('rpi-gpio');
 
 //gpio.setup(7, gpio.DIR_OUT);
-gpio.setup(4, gpio.DIR_IN, readInput);
- 
-function readInput() {
-    gpio.read(4, function(err, value) {
-        console.log('The value is ' + value);
-    });
-}
+gpio.setup(7, gpio.DIR_IN, readInput);
 
 app.set('view engine', 'ejs');
 
@@ -18,12 +12,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 console.log(path.join(__dirname, 'public'));
 
+function readInput(err) {
+    if (err) throw err;
+    gpio.read(7, function(err, value) {
+        if (err) throw err;
+        console.log('The value is ' + value);
+    });
+};
+
 app.get('/', function(req, res){ 
-    gpio.read(4, function(err, value) {
+    gpio.read(7, function(err, value) {
+        if (err) throw err;
         console.log('The value is ' + value);
         return res.render('index', {status: value});
     });
 });
+
 /*
 app.get('/', function(req, res){ 
   res.render('index',{status:"Press Button To change Status of Led !!"});
