61c8c6e9be52190a1eea902f93ed33a791473745
Mihaela <mihaelaagrigore@gmail.com>
2018-03-19 21:13:49 +0200

    gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var gpio = require('rpi-gpio');
 
 //gpio.setup(7, gpio.DIR_OUT);
-gpio.setup(7, gpio.DIR_IN, readInput);
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 app.set('view engine', 'ejs');
 
@@ -12,23 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 console.log(path.join(__dirname, 'public'));
 
-function readInput(err) {
-    if (err) throw err;
-    gpio.read(7, function(err, value) {
-        if (err) throw err;
-        console.log('The value is ' + value);
-    });
-};
-
-app.get('/', function(req, res){ 
-    gpio.read(7, function(err, value) {
-        if (err) throw err;
-        console.log('The value is ' + value);
-        //return res.render('index', {status: value});
-    });
-    gpio.on('change', function(channel, value) {
-      console.log('Channel ' + channel + ' value is now ' + value);
-    });
+gpio.on('change', function(channel, value) {
+  console.log('Channel ' + channel + ' value is now ' + value);
 });
 
 /*
