f4265fd79955dac77ed95c8f40f0877f4c80d2d8
Mihaela <mihaelaagrigore@gmail.com>
2018-03-21 20:15:46 +0200

    pin 11 write

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var app = express();
 var path = require('path');
 var gpio = require('rpi-gpio');
 
+gpio.setup(11, gpio.DIR_OUT);
 gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 gpio.on('change', function(channel, value) {
   console.log('Channel ' + channel + ' value is now ' + value);
+  gpio.write(11, value, function(err) {
+        if (err) throw err;
+        console.log('Written " + value + " to pin 11');
+    });
 });
 
 
