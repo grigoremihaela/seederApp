57b8bc752f5af81e0cf06975285cd5ddf17626b5
Mihaela <mihaelaagrigore@gmail.com>
2018-03-21 19:09:26 +0200

    first commit

--- index.js
+++ index.js
@@ -4,16 +4,31 @@ var path = require('path');
 var gpio = require('rpi-gpio');
 
 //gpio.setup(7, gpio.DIR_OUT);
-gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(7, gpio.DIR_IN, readInput);
 
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//console.log(path.join(__dirname, 'public'));
+console.log(path.join(__dirname, 'public'));
 
-gpio.on('change', function(channel, value) {
-  console.log('Channel ' + channel + ' value is now ' + value);
+function readInput(err) {
+    if (err) throw err;
+    gpio.read(7, function(err, value) {
+        if (err) throw err;
+        console.log('The value is ' + value);
+    });
+};
+
+app.get('/', function(req, res){ 
+    gpio.read(7, function(err, value) {
+        if (err) throw err;
+        console.log('The value is ' + value);
+        return res.render('index', {status: value});
+    });
+    gpio.on('change', function(channel, value) {
+      console.log('Channel ' + channel + ' value is now ' + value);
+    });
 });
 
 /*
