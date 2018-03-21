06ead3b62b66085ed4d81bdabba6b810914d7354
Mihaela <mihaelaagrigore@gmail.com>
2018-03-21 17:44:49 +0200

    gpio on change

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var gpio = require('rpi-gpio');
 
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+
 gpio.on('change', function(channel, value) {
   console.log('Channel ' + channel + ' value is now ' + value);
 });
-gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+
