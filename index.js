e275ca842b647ee9e9686daffd57d9973d0caa8e
grigoremihaela <grigoremihaela@ymail.com>
2018-03-27 19:41:23 +0300

    get time bob

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ gpio.on('change', function(channel, value) {
     console.log('ON ' + countON);
   } else {
     if (control) {
-      var timeEnd = startTime.getTime();  // milliseconds (1 ms = 0.001 sec)
-      console.log('OFF    ' + timeEnd/1000 + ' sec.    ' + countPIN + ' total'); 
+      var timeEnd = new Date().getMilliseconds() - startTime.getMilliseconds();  // milliseconds (1 ms = 0.001 sec)
+      console.log('OFF    ' + timeEnd + ' milliseconds      ' + countPIN + ' total'); 
       console.log('start date ' + startTime.getMilliseconds()); 
       console.log('end date ' + new Date().getMilliseconds()); 
       countON = 0
